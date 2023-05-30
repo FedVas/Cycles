6ed09b5e2ef4232bac57d33ea39e079c64e5d881
Fedor <atlz89@mail.ru>
2023-05-31 01:50:13 +0300

    Parametrized test Csv

--- src/test/java/ru/netology/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -2,29 +2,22 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class VacationServiceTest {
-    @Test
-    public void salary10_000() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/Vacation.csv")
+    public void salary10_000(int exepted, int income, int expenses, int threshold) {
         VacationService service = new VacationService();
-        int exepted = 3;
-        int actual = service.calculate(10_000, 3000, 20_000);
+        //     int exepted = 3;
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(exepted, actual);
 
     }
-
-    @Test
-    public void salary100_000() {
-        VacationService service = new VacationService();
-        int exepted = 2;
-        int actual = service.calculate(100_000, 60000, 150_000);
-
-        Assertions.assertEquals(exepted, actual);
-
-    }
-
 }
+
 
 
 
